dd0883ba7c33c64e3843e6d5f3cb66a4ccc5583e
Mevin Varghese <mevinvarghese2000@gmail.com>
2024-11-18 11:57:05 +0530

    completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //localhost:4200/
+  {
+    path: '',
+    component: LoginComponent,
+  },
+  //localhost:4200/register
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  //localhost:4200/dashboard
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
